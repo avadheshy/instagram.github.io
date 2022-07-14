d2e3f18351965ad677ca51f4c877d8a26fe3a0b2
avadheshy <avadheshy2015@gmail.com>
2022-07-14 15:42:58 +0530

    login page added

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app.models import user
+from app.models import person
 
 # Register your models here.
-admin.site.register(user)
+admin.site.register(person)
--- app/views.py
+++ app/views.py
@@ -1,19 +1,40 @@
-from django.shortcuts import render
-from app.models import user
+from email import message
+from django.shortcuts import redirect, render
+from app.models import person
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-def register(request):
+    return render(request, 'index.html')
+def login(request):
     if request.method=='POST':
-        user_data=request['user_data']
-        first_name=request['first_name']
-        last_name=request['last_name']
-        password= request['password']
-        #person=user(user_data=user_data,first_name=first_name
-        #last_name=last_name,password=password)
-        #person.save()
+        print('inside the post method')
+        user_data=request.POST['username']
+        password = request.POST['password']
+        print('before login page')
+        if person.objects.filter(user_data=user_data).exists():
+            print('login page')
+            return render(request,'login.html',{'name':user_data})
+        else:
+            print('home page')
+            return redirect('/')
+
+def register(request):
+    if request.method == 'POST':
+        user_data = request.POST['user_data']
+        first_name = request.POST['first_name']
+        last_name = request.POST.get('last_name',False)
+        password = request.POST['password']
+        if person.objects.filter(user_data=user_data).exists():
+            messages.info(request,'user allready exist')
+            return redirect('register')
+        else:
+            obj = person(user_data=user_data, first_name=first_name,
+                        last_name=last_name, password=password)
+            obj.save()
+            return redirect('/')
+        
 
     else:
         return render(request, 'register.html')
